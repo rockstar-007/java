38494dd4780711d45ae3aa189b9cf9cb9501ed07
Abhijith Rauth <rauthabhijith@gmail.com>
2025-02-20 19:32:44 +0530

    Create Details.java
    TO show how variable works

---
+++ Details.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Details{
+
+	public static void main(String[] args){
+		
+		Scanner Apple = new Scanner(System.in);		
+		System.out.println("Enter your name:");
+		String name = Apple.next();
+		System.out.println("hi " + name);
+		}
+
+
+}
