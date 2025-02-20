2a7996713b6fec82a6ea4d6a0b05f2c294215fe9
Abhijith Rauth <rauthabhijith@gmail.com>
2025-02-20 21:21:43 +0530

    Update Data types.java
    Data Types in java

--- Data types.java
+++ Data types.java
@@ -4,16 +4,24 @@ class DataTypes{
 		int num = 5;
 		String Letter = "abhijith";
 		char Text = 'a';
-		float value = 8.6f;
+		float value = 123.652858f;
 		boolean bool = true;
-		
+		byte A = 123;
+		short B = 12345;
+		long C = 123456789l;
+		double D = 1237.123456789d;
+		char E = 66;
+
 		System.out.println(num);
 		System.out.println(Letter);
 		System.out.println(Text);
 		System.out.println(value);
 		System.out.println(bool);
 		System.out.println("hi"+Letter);
-
-}		
-	
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println(C);
+		System.out.println(D);
+		System.out.println(E);
+}	
 }
