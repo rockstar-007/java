2ed4ba1a1ea32b711d31d4cc861e23060ea6cc99
Abhijith Rauth <rauthabhijith@gmail.com>
2025-02-20 20:59:53 +0530

    Create Data types.java
    TO show different datatypes

---
+++ Data types.java
@@ -0,0 +1,19 @@
+class DataTypes{
+	public static void main(String[] args){
+		
+		int num = 5;
+		String Letter = "abhijith";
+		char Text = 'a';
+		float value = 8.6f;
+		boolean bool = true;
+		
+		System.out.println(num);
+		System.out.println(Letter);
+		System.out.println(Text);
+		System.out.println(value);
+		System.out.println(bool);
+		System.out.println("hi"+Letter);
+
+}		
+	
+}
