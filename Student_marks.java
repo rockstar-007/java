84983bf71ab44dff2dbecd3bfee4fc67efc33950
Abhijith Rauth <rauthabhijith@gmail.com>
2025-03-02 14:49:29 +0530

    Create Student_marks.java
    Displaying Student name , marks

---
+++ Student_marks.java
@@ -0,0 +1,12 @@
+class check{
+	public static void main(String[] args){
+		String name;
+		name ="Abhijth";
+		System.out.println(name);
+		int  marks = 300;
+		String Details = name +"\n marks :"+ marks;
+		System.out.println(Details);
+		
+		
+}
+}
