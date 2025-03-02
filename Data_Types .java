663785a47a9147c42a8e23b67e76200fabc879ca
Abhijith Rauth <rauthabhijith@gmail.com>
2025-03-02 14:23:47 +0530

    Create Data_Types .java
    Basics data types for variables

---
+++ Data_Types .java
@@ -0,0 +1,15 @@
+class PrDataTypes{
+	public static void main(String[] args){
+		int Num = 5;
+		float floatNum = 3.4f;
+		char Letter = 'A';
+		boolean Bool =true ; 
+		String Text = "my name is Abhijith";
+		System.out.println(Num);
+		System.out.println(floatNum);
+		System.out.println(Letter);
+		System.out.println(Bool);
+		System.out.println(Text);
+}
+
+}
